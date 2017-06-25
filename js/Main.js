59503f01602ff370acd3a3d6a74c752f27902fd4
Victor Chen <vsyc@cin.ufpe.br>
2017-06-25 14:18:12 -0300

    Test GITHUB path to file

--- js/Main.js
+++ js/Main.js
@@ -11,10 +11,10 @@ var line = new LineGraph("lineGraph", ((width/3)-(width/50)), height/2);
 var dataset = getData(filePath);
 
 d3.json("../json/forbes2016.json", function(d){
-	map.setData(d.customers)
+  map.setData(d.customers)
 })
 d3.json("../json/world-topo-min.json", function(d){
-	map.setMap(topojson.feature(d,d.objects.countries).features);
+  map.setMap(topojson.feature(d,d.objects.countries).features);
 })
 
 pie.setData("../test/pieTest.csv");
@@ -30,14 +30,14 @@ line.setData("../test/lineTest.csv");
 
 var disp = d3.dispatch("countrySelected");
 disp.on("countrySelected", function(){
-	if(this.caller === "map"){
-		pie.setData(this.data);
-		list.setData(this.data);
-		scatterplot.setData(this.data);
-		line.setData(this.data);
-	}
+  if(this.caller === "map"){
+    pie.setData(this.data);
+    list.setData(this.data);
+    scatterplot.setData(this.data);
+    line.setData(this.data);
+  }
 });
 
 function getData(filePath){
-	return [];
+  return [];
 }
--- js/WorldMap.js
+++ js/WorldMap.js
@@ -1,98 +1,95 @@
 class WorldMap {
-	constructor(id, w, h){
-		var zoom = d3.zoom()
-			.scaleExtent([1,8])
-			.translateExtent([[-100, -100], [width + 90, height + 100]])
-			.on("zoom",zoomed);
-		this.canvas = d3.select("#"+id)
-			.attr("class", "container")
-			.attr("width",w-20)
-			.attr("height",h)
-			.attr("transform","translate(0,60)")
-			.call(zoom);
-		this.id = id;
-		this.w = w;
-		this.h = h;
+  constructor(id, w, h){
+    var zoom = d3.zoom()
+      .scaleExtent([1,8])
+      .translateExtent([[-100, -100], [width + 90, height + 100]])
+      .on("zoom",zoomed);
+    this.canvas = d3.select("#"+id)
+      .attr("class", "container")
+      .attr("width",w-20)
+      .attr("height",h)
+      .attr("transform","translate(0,60)")
+      .call(zoom);
+    this.id = id;
+    this.w = w;
+    this.h = h;
 
-		this.tooltip = this.canvas
-			.append("div")
-			.attr("class", "tooltip hidden");
+    this.tooltip = this.canvas
+      .append("div")
+      .attr("class", "tooltip hidden");
 
-		this.cScale = d3.scaleLinear()
-			.range(["#0f0", "#00f"]);
-			
-		this.projection = d3.geoMercator().translate([w/2,height/2]).scale(width / 2 / Math.PI);
-		this.path = d3.geoPath().projection(this.projection);
-
-		var that = this;
-		function zoomed(){
-			that.canvas.selectAll(".country").attr("transform",d3.event.transform);
-		}
-	}
-
-	setMap(data){
-		var that = this;
-				
-		this.cScale.domain([0,that.maxScore]);
-
-		var country = this.canvas.selectAll(".country").data(data);
-		country.enter()
-			.insert("path")
-			.attr("class", "country")
-			.attr("d",that.path)
-			.attr("id", function(d,i){return d.properties.name;})
-			.style("fill",function(d){
-				if(that.countries.indexOf(d.properties.name) == -1){
-					return "grey";
-				}else{
-					return that.cScale(that.score[that.countries.indexOf(d.properties.name)]);
-				}
-			})
-			//.style("fill",function(d){return that.cScale(d.rank);})
-			.on("mouseover",function(d,i){that.tooltip.classed("hidden", false).attr("style", "x:"+d3.mouse(this)[0]+"px;y:"+d3.mouse(this)[1]+"px;").html(d.properties.name);})
-			.on("mouseout",function(d){that.tooltip.classed("hidden", true);});
-			
-	}
+    this.cScale = d3.scaleLinear()
+      .range(["#0f0", "#00f"]);
 	
-	rankCalc(val) {
-		if(val < 10){ 
-			return 11;
-		}else if(val < 20){ 
-			return 6;
-		}else if(val < 50){ 
-			return 5;
-		}else if(val < 100){ 
-			return 4;
-		}else if(val < 200){ 
-			return 3;
-		}else if(val < 500){ 
-			return 2;
-		}else if(val < 1000){ 
-			return 2;
-		}else{ 
-			return 1;
-		}
-	}
-	
-	setData(data){
-		var that = this;
-		this.countries = [];
-		this.score = [];
-		this.maxScore = 0;
-		for (var i = 0; i < data.length; i++){
-			var a = this.countries.indexOf(data[i].country);
-			var b = this.rankCalc(data[i].rank)
-			if(a == -1){ 
-				this.countries.push(data[i].country); 
-				this.score.push(b)
-				if(b > this.maxScore){ this.maxScore = b }
-			}else{ 
-				this.score[a] += b; 
-				if(this.score[a] > this.maxScore){ this.maxScore = this.score[a]}
-			}				
-		}
-		console.log(this.countries)
-		console.log(this.score)
-	}
+    this.projection = d3.geoMercator().translate([w/2,height/2]).scale(width / 2 / Math.PI);
+    this.path = d3.geoPath().projection(this.projection);
 
+    var that = this;
+    function zoomed(){
+      that.canvas.selectAll(".country").attr("transform",d3.event.transform);
+    }
+  }
+
+  setMap(data){
+    var that = this;
+
+    this.cScale.domain([0,that.maxScore]);
+
+    var country = this.canvas.selectAll(".country").data(data);
+    country.enter()
+      .insert("path")
+      .attr("class", "country")
+      .attr("d",that.path)
+      .attr("id", function(d,i){return d.properties.name;})
+      .style("fill",function(d){
+        if(that.countries.indexOf(d.properties.name) == -1){
+          return "grey";
+        }else{
+          return that.cScale(that.score[that.countries.indexOf(d.properties.name)]);
+        }
+      })
+      //.style("fill",function(d){return that.cScale(d.rank);})
+      .on("mouseover",function(d,i){that.tooltip.classed("hidden", false).attr("style", "x:"+d3.mouse(this)[0]+"px;y:"+d3.mouse(this)[1]+"px;").html(d.properties.name);})
+      .on("mouseout",function(d){that.tooltip.classed("hidden", true);});
+  }
+	
+  rankCalc(val) {
+    if(val < 10){ 
+      return 11;
+    }else if(val < 20){ 
+      return 6;
+    }else if(val < 50){ 
+      return 5;
+    }else if(val < 100){ 
+      return 4;
+    }else if(val < 200){ 
+      return 3;
+    }else if(val < 500){ 
+      return 2;
+    }else if(val < 1000){ 
+      return 2;
+    }else{ 
+      return 1;
+    }
+  }
+
+  setData(data){
+    var that = this;
+    this.countries = [];
+    this.score = [];
+
+    this.maxScore = 0;
+    for (var i = 0; i < data.length; i++){
+      var a = this.countries.indexOf(data[i].country);
+      var b = this.rankCalc(data[i].rank)
+      if(a == -1){ 
+        this.countries.push(data[i].country); 
+        this.score.push(b)
+        if(b > this.maxScore){ this.maxScore = b }
+      }else{ 
+        this.score[a] += b; 
+        if(this.score[a] > this.maxScore){ this.maxScore = this.score[a]}
+      }				
+    }
+  }
 }
