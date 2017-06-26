197b368f74842ba1280c684024e2121ee2d9930d
Victor Sin Yu Chen <vsyc@WINDOWS.CIN.UFPE.BR>
2017-06-26 16:33:43 -0300

    Cleaning Main.js. Changing PieGraph to PieChart

--- js/Main.js
+++ js/Main.js
@@ -1,43 +1,35 @@
 var width = window.innerWidth-100;
 var height = window.innerHeight-50;
-var filePath = "../json/";
-
-var map = new WorldMap("map", width, height-50);
-var list = new InfoList("list", width, (height/2)-50);
-var pie = new PieGraph("pieGraph", ((width/3)-(width/50)), height/2);
-var scatterplot = new Scatterplot("scatterplot", ((width/3)-(width/50)), height/2);
-var line = new LineGraph("lineGraph", ((width/3)-(width/50)), height/2);
-
-var dataset = getData(filePath);
-
-d3.json("../json/forbes2016.json", function(d){
-  map.setData(d.customers)
-})
-d3.json("../json/world-topo-min.json", function(d){
-  map.setMap(topojson.feature(d,d.objects.countries).features);
-})
-
-pie.setData("../test/pieTest.csv");
-list.setData("../test/listTest.csv")
-scatterplot.setData("../test/scatterplotTest.csv");
-line.setData("../test/lineTest.csv");
-
-//map.setData(dataset);
-//pie.setData(dataset);
-//list.setData(dataset);
-//scatterplot.setData(dataset);
-//line.setData(dataset);
 
 var disp = d3.dispatch("countrySelected");
-disp.on("countrySelected", function(){
-  if(this.caller === "map"){
-    pie.setData(this.data);
-    list.setData(this.data);
-    scatterplot.setData(this.data);
-    line.setData(this.data);
+disp.on("countrySelected", function() {
+  if(this.caller === "map") {
+    list.attData(this.data_list);
+    pie.attData(this.data_pie);
+    scatterplot.attData(this.data_scatter);
+    line.attData(this.data_line);
   }
 });
 
-function getData(filePath){
-  return [];
-}
+var map = new WorldMap("map", width, height-50);
+var list = new InfoList("list", width, (height/2)-50);
+var pie = new PieChart("pie", ((width/3)-(width/50)), height/2);
+var scatterplot = new Scatterplot("scatter", ((width/3)-(width/50)), height/2);
+var line = new LineGraph("line", ((width/3)-(width/50)), height/2);
+
+// datasets
+d3.json("https://raw.githubusercontent.com/vsychen/Big-2000-visualizacao-2017-1/master/json/forbes.json", function(d) {
+  var dataset = d.companies;
+  console.log(dataset);
+
+  map.setData(dataset);
+  //list.setData(dataset);
+  //pie.setData(dataset);
+  //scatterplot.setData(dataset);
+  //line.setData(dataset);
+
+  // world map
+  d3.json("https://rawgit.com/vsychen/Big-2000-visualizacao-2017-1/master/json/world-topo-min.json", function(d){
+    map.setMap(topojson.feature(d,d.objects.countries).features);
+  });
+});
